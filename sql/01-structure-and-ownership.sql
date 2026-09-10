bda1f1feb6bb1d9ad21413d19431b9d191bcc944
LEA FRANCOIS <lea.jeane.francois@gmail.com>
2026-09-10 15:10:35 +0200

    feat: add scripts

---
+++ sql/01-structure-and-ownership.sql
@@ -0,0 +1,20 @@
+-- ============================================================
+-- 01-structure-and-ownership.sql
+-- Ce script doit être rejouable en entier sur une base vide.
+-- ============================================================
+
+-- ------------------------------------------------------------
+-- US 1 LA BASE ET SES ACCES : Connecté en tant que postgres (superutilisateur)
+-- Objectif : créer la base et les deux rôles
+-- À exécuter avec : psql -U postgres -d postgres -v ON_ERROR_STOP=1 -h localhost -p 5432 -f 01-structure-and-ownership.sql
+-- ------------------------------------------------------------
+
+CREATE DATABASE disney_trombifunscope;
+
+CREATE ROLE trombifunscope_admin LOGIN PASSWORD 'ILoveDisney'; 
+
+CREATE ROLE trombifunscope_member LOGIN PASSWORD 'MeTooILoveDisney';
+
+ALTER DATABASE disney_trombifunscope OWNER TO trombifunscope_admin;
+
+\c disney_trombifunscope trombifunscope_admin
---
+++ sql/02-create-table.sql
@@ -0,0 +1,29 @@
+-- ============================================================
+-- 02-create-table.sql
+-- Ce script doit être rejouable en entier sur une base vide.
+-- psql -U trombifunscope_admin -d disney_trombifunscope -v ON_ERROR_STOP=1 -h localhost -p 5432 -f 02-create-table.sql
+-- ============================================================
+
+-- ------------------------------------------------------------
+-- US 2 TABLE DES FICHES: Connecté en trombifunscope - création des tables
+-- Opération de structure => administrateur
+-- ------------------------------------------------------------
+
+CREATE TABLE disney_characters(
+   student_id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+   lastname VARCHAR(30) NOT NULL,
+   firstname VARCHAR(20) NOT NULL,
+   disney_character VARCHAR(50) NOT NULL,
+   disney_movie VARCHAR(50) NOT NULL,
+   iconic_quote VARCHAR(50),
+   compagnon VARCHAR(30),
+   image_url TEXT NOT NULL,
+   creation_date TIMESTAMP NOT NULL
+);
+
+-- ------------------------------------------------------------
+-- Connecté en trombifunscope_admin - attribution des droits
+-- Rappel du besoin : member = lecture+écriture sur disney_characters,
+-- ------------------------------------------------------------
+
+GRANT SELECT, INSERT, UPDATE, DELETE ON disney_characters TO trombifunscope_member;
