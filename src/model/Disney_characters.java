baf98339489404c72917e1fc41f6e7a0bb2f6816
Thibaut CABON <thibautnoback@hotmail.com>
2026-09-11 11:53:44 +0200

    feat: adding table (disney_characters) column in the java class (DisneyCharactersDao)

--- src/model/Disney_characters.java
+++ src/model/Disney_characters.java
@@ -7,10 +7,29 @@ package model;
  */
 public class Disney_characters {
 
-    private int id;
     private String firstName;
     private String lastName;
+    private String disney_character;
+    private String disney_movie;
+    private String iconic_quote;
+    private String compagnon;
+    private String image_url;
 
-    public Disney_characters() {
+    public Disney_characters(String firstName,
+                             String lastName,
+                             String disney_character,
+                             String disney_movie,
+                             String iconic_quote,
+                             String compagnon,
+                             String image_url
+    ) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.disney_character = disney_character;
+        this.disney_movie = disney_movie;
+        this.iconic_quote = iconic_quote;
+        this.compagnon = compagnon;
+        this.image_url = image_url;
     }
+
 }
